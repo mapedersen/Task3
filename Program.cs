6dba98a11e6ab2db4bac918893483edc226e18e8
marcus pedersen <marcus@mpedersen.me>
2024-05-28 23:43:02 +0200

    set age through personHandler

--- Program.cs
+++ Program.cs
@@ -6,20 +6,21 @@
         {
             try
             {
+                
                 Person person = new Person
                 {
                     FName = "Marcus",
                     LName = "Pedersen"
                 };
-
+                PersonHandler personHandler = new PersonHandler();
+                personHandler.SetAge(person, 27);
                 Console.WriteLine($"{person.FName} {person.LName} is {person.Age} years old");
+
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            PersonHandler personHandler = new PersonHandler();
-            personHandler.SetAge(person, 15);
 
         }
     }
