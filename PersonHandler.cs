1a37d6b54b7bd921b6fee56571a181c0d89a492a
marcus pedersen <marcus@mpedersen.me>
2024-05-28 23:46:39 +0200

    Methods to handle all operations on person

--- PersonHandler.cs
+++ PersonHandler.cs
@@ -8,10 +8,29 @@ namespace Task3
 {
     internal class PersonHandler
     {
+        // Methods to handle operations on person
         public void SetAge(Person person, int age)
         {
             person.Age = age;
         }
+        public void SetFirstName(Person person, string firstName)
+        {
+            person.FName = firstName;
+        }
+        public void SetLastName(Person person, string lastName)
+        {
+            person.LName = lastName;
+        }
+        public void SetHeight(Person person, double height)
+        {
+            person.Height = height;
+        }
+        public void setWeight(Person person, double weight)
+        {
+            person.Weight = weight;
+        }
+
+        // Method to create a person object
         public Person CreatePerson(int age, string fName, string lName, double height, double weight)
         {
             Person person = new Person
