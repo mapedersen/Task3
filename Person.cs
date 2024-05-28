12628e5a929c99ad5de498f3c40c0fe767257352
marcus pedersen <marcus@mpedersen.me>
2024-05-28 15:46:51 +0200

    Create private fields for Person class

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Person
+    {
+        private int _age;
+        private string _fName;
+        private string _lName;
+        private int _height;
+        private int _weight;
+    }
+}
