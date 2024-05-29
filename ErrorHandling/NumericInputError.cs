6bb946860040299ea077d85cb7d1805c596da5bf
marcus pedersen <marcus@mpedersen.me>
2024-05-29 15:39:24 +0200

    Moved errorhandling to separate folder

---
+++ ErrorHandling/NumericInputError.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3.ErrorHandling
+{
+    internal class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            throw new Exception("You tried to use a text\r\ninput in a numeric only field. This fired an error!");
+        }
+    }
+}
---
+++ ErrorHandling/TextInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.ErrorHandling
+{
+    internal class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            throw new Exception("You tried to use a text input in a numeric only field. This fired an error!");
+        }
+    }
+}
