218482969bd88629443febb16dad31a002e92c64
marcus pedersen <marcus@mpedersen.me>
2024-05-29 15:29:42 +0200

    Extracted NumerInputError from UserError

---
+++ NumericInputError.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3
+{
+    internal class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            throw new Exception("“You tried to use a text\r\ninput in a numeric only field. This fired an error!");
+        }
+    }
+}
