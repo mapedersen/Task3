dde26a8241fbb9debe246ac4910817c59c9b7c10
marcus pedersen <marcus@mpedersen.me>
2024-05-29 15:54:13 +0200

    Create abstract class Animal

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+
+        internal abstract void DoSound();
+
+        public Animal(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }   
+    }
+}
