adfbd2d28f0bd7bb32cd525fa8992322d7126b8a
marcus pedersen <marcus@mpedersen.me>
2024-05-31 22:31:56 +0200

    Create derived class Dog

---
+++ Animals/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Animals
+{
+    internal class Dog : Animal
+    {
+        public bool IsAPet { get; set; }
+        public Dog(string name, int age, double weight) : base(name, age, weight)
+        {
+            IsAPet = true;
+        }
+        internal override void DoSound()
+        {
+            Console.WriteLine("WoffWoff");
+        }
+    }
+}
