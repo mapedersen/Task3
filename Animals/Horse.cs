ff4b6c0d3b7657e28ad59e3369932628cbc1cb42
marcus pedersen <marcus@mpedersen.me>
2024-05-31 22:25:20 +0200

    Implement derived class Horse

---
+++ Animals/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Animals
+{
+    internal class Horse : Animal
+    {
+        public bool IsRideAble { get; set; }
+        public Horse(string name, int age, double weight) : base(name, age, weight) 
+        { 
+        }
+        internal override void DoSound()
+        {
+            Console.WriteLine("geeeehegeeeehegeeeehe");
+        }
+    }
+}
